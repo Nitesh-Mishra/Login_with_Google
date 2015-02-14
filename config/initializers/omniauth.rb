eaafc3a1e232bc702f3956ab1c42ba405c51978e
Nitesh-Mishra <nitesh.mishra143@gmail.com>
2015-02-14 23:28:16 +0530

    Remove the api key and secret key.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :google_oauth2, '3887905343-l2n5kn9ikdd858238tul6kppi13aprfb.apps.googleusercontent.com', 'iXe52Mb5NIheeJv7-INCwcZg', {
+	provider :google_oauth2, 'google_api_key', 'google_secret_key', {
   scope: ['email','https://www.googleapis.com/auth/gmail.modify'],access_type: 'offline'}	
 end
